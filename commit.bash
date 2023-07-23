2d3eb5e306177867060a7481c725bd718458fb92
Partha Sarathi Dey <131694386+parthasdey2304@users.noreply.github.com>
2023-07-23 16:17:29 +0530

    Create commit.bash

---
+++ commit.bash
@@ -0,0 +1,16 @@
+clear
+# this shows the list of the files that have been added or updated or removed or modified
+figlet STATUS
+git status
+
+# this adds them to be commited
+figlet Adding . . . . . 
+git add . 
+
+# this commits the files
+figlet Commiting . . . . . 
+git commit -m "initial commit" 
+
+# this pushes the code to the main branch of the repository
+figlet Uploaded ! !
+git push 
