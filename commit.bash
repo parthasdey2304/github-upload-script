f6ab530371fd3e40fbcecdc93ae955881295f74c
Partha Sarathi Dey <131694386+parthasdey2304@users.noreply.github.com>
2023-07-23 16:30:12 +0530

    Update commit.bash

--- commit.bash
+++ commit.bash
@@ -1,16 +1,16 @@
 clear
 # this shows the list of the files that have been added or updated or removed or modified
-figlet STATUS
+figlet S T A T U S
 git status
 
 # this adds them to be commited
-figlet Adding . . . . . 
+figlet A d d i n g  . . . . . 
 git add . 
 
 # this commits the files
-figlet Commiting . . . . . 
+figlet C o m m i t i n g . . . . . 
 git commit -m "initial commit" 
 
 # this pushes the code to the main branch of the repository
-figlet Uploaded ! !
+figlet U p l o a d e d ! !
 git push 
