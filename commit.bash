e16dd536b635f094f9c73398c1e45499f4bfcf1c
Partha Sarathi Dey <131694386+parthasdey2304@users.noreply.github.com>
2023-10-08 15:34:44 +0530

    Update commit.bash

--- commit.bash
+++ commit.bash
@@ -1,16 +1,38 @@
 clear
-# this shows the list of the files that have been added or updated or removed or modified
-figlet S T A T U S
-git status
+figlet "Git!"
 
-# this adds them to be commited
-figlet A d d i n g  . . . . . 
-git add . 
+# Check if .git directory exists
+if [ -e '.git' ]; then
+    # Git directory exists
+    echo -e '\e[32m[INFO] Here are all the unstaged files...\e[0m'
+    git status
+    echo ''
+    echo ''
 
-# this commits the files
-figlet C o m m i t i n g . . . . . 
-git commit -m "initial commit" 
+    # Pull any changes from the remote repository
+    echo -e '\e[32m[INFO] Checking if any files need to be pulled...\e[0m'
+    git pull
 
-# this pushes the code to the main branch of the repository
-figlet U p l o a d e d ! !
-git push 
+    # Add all files for staging
+    echo -e '\e[32m[INFO] Adding all the files...\e[0m'
+    git add .
+
+    # Commit the staged files
+    echo -e '\e[32m[INFO] Committing files...\e[0m'
+    git commit -m "Initial commit"
+
+    # Push the committed changes to the remote repository
+    echo -e '\e[32m[INFO] Pushing files...\e[0m'
+    git push --force
+
+    # Add a 1-second delay for visual effect
+    sleep 1
+
+    # Clear the screen and display "Repo Updated" message
+    clear
+    figlet "Repo Updated!!"
+else
+    # Git directory does not exist
+    echo -e '\e[31mFatal Error: Git not initialized!'
+    echo -e '\e[31m.git file not found!\e[0m'
+fi
